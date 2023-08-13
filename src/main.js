829ac1b10a344b09b574e5e0f67ae2a553174fc9
Koranteng Anane <squidiw.squidiw@gmail.com>
2023-08-13 14:16:05 -0400

    Edit 1.0.3
    Added liquid-ajax-cart package

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 /**
  * imports
  */
+//AlpineJS Imports
 import Alpine from 'alpinejs'
 import AlpineGlobals from './alpine/index.js'
+
+//Liquid-ajax-cart(https://liquid-ajax-cart.js.org/guide/)
+import 'liquid-ajax-cart';
 
 import './css/main.css'
 import './components/cart'
